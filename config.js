c47f91570b454bed28b58867364100f42d03ce39
Adzy <adzy.frx@gmail.com>
2025-10-28 07:19:17 +0800

    fix typo

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ global.pkg = pkg;
 global.qtext = {
 	key: {
 		remoteJid: "status@broadcast",
-		participant: "18002428478@s.whatsapp.net",
+		participant: "18002428478@s.whatsapp.net"
 	},
-	message: { extendedTextMessage: { text: "Simple WeA bot by Dzy" } },
+	message: { extendedTextMessage: { text: "Simple WeA bot by Dzy" } }
 };
-global.readws = {
+global.readsw = {
 	active: true,
 	react: false,
-	emoji: ["🔥", "💀", "☠️", "🥀", "🥶"],
-}
+	emoji: ["🔥", "💀", "☠️", "🥀", "🥶"]
+};
 /*============= Konfigurasi =============*/
 global.cfg = {
 	pairing: 962796121703 /* [ Nomor bot ] */,
@@ -25,13 +25,13 @@ global.cfg = {
 	tz: "Asia/Jakarta" /* [ Atur aja ] */,
 	zone: "WIB",
 	user: {
-		limit: 50,
+		limit: 50
 	},
 	bot: {
 		name: "FanzWeA bot" /* [ Nama bot ] */,
 		owner: [
 			"962796121703",
-			"6283899616999",
+			"6283899616999"
 		] /* [ Klo mau tambah tinggal [ ...03', '62x' ] ] */,
 		ownerUrl: "https://nefu.life/adzy" /* [ Bebas mau link apah ] */,
 		version: global.pkg.version,
@@ -41,12 +41,12 @@ global.cfg = {
 		thumbvid: "" /* [ Buat versi video nya bisa pke url atau buffer ] */,
 		audio: readFileSync("./media/pedih.mp3"),
 		self: false /* [ default status bot] */,
-		on: true,
+		on: true
 	},
 	ads: {
 		title: "Fanz-WeA boT",
 		body: "Fanzbot a simple WeA Bot",
-		source: "https://kua.lat/ikamLah",
+		source: "https://kua.lat/ikamLah"
 	},
 	mess: {
 		wait: "`[-]` Tunggu sebentar yah, sekitar 3 tahun lagi..." /* [ Pesan tunggu ] */,
@@ -58,16 +58,16 @@ global.cfg = {
 		private:
 			"`[×]` Hanya bisa di private chat" /* [ Pesan bot hanya dipakai di pc ] */,
 		premium: "`[×]` Dibuat untuk user prem" /* [ Pesan khusus premium ] */,
-		free: "`[!]` Anda user premium dilarang pakai" /* [ Ada Ada saja😅 ] */,
+		free: "`[!]` Anda user premium dilarang pakai" /* [ Ada Ada saja😅 ] */
 	},
 	s: {
 		pack: "" /* [ setiker pack ] */,
-		auth: "[!] Aku mau Makan Roti Belah Dua" /* [ Author nya ] */,
+		auth: "[!] Aku mau Makan Roti Belah Dua" /* [ Author nya ] */
 	},
 	apiKey: {
 		ryhar: "p2pe75mrois182180i6y" /* [ buat naro macam macam apiKey taro di sini aja biar enakh ] */,
-		gemini: "AIzaSyAFZuYs9iu4lRrR_cWY8jelar9CUMyvL5o" /* [ Ambil di ai.google.studio katanya ] */,
-	},
+		gemini: "AIzaSyAFZuYs9iu4lRrR_cWY8jelar9CUMyvL5o" /* [ Ambil di ai.google.studio katanya ] */
+	}
 };
 
 // Hot reload config.js ketika ada perubahan
